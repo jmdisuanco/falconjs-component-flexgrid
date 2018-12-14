d7fd8dcfde12e4acaf29288dd0827fc185be8944
= <=>
2018-12-14 17:15:01 +0400

    implemented Row Component

--- src/flexgrid/Col.js
+++ src/flexgrid/Col.js
@@ -1,16 +1,12 @@
 import {h} from '@falconjs.io/falcon'
 import s from './style/index.scss';
-import PropTypes from 'prop-types';
 
-const stringOrNumber = PropTypes.oneOfType([PropTypes.string, PropTypes.number]);
-const numberOrObject = PropTypes.oneOfType([PropTypes.number, PropTypes.object]);
-
-let  Col = ({ span, offset, className, children, prefix = 'flcn-grid-col', order, ...others }) => {
+let  Col = ({ span, offset, children, prefix = 'flcn-grid-col', order, ...others }) => {
   
   let classes=''
-
-  ['xs', 'sm', 'md', 'lg', 'xl'].forEach((size) => {
-      if (others[size]) {
+  let sizes = ['xs', 'sm', 'md', 'lg', 'xl']
+  sizes.map( (size)=>{
+     if (others[size]) {
         let sizeParams = {};
         if (typeof others[size] === 'number') {
           sizeParams.span = others[size];
@@ -22,28 +18,12 @@ let  Col = ({ span, offset, className, children, prefix = 'flcn-grid-col', order
         sizeParams.offset || sizeParams.offset === 0 ? classes += `${prefix}-${size}-offset-${sizeParams.offset} ` : null
         sizeParams.order || sizeParams.order === 0? classes += `${prefix}-${size}-order-${sizeParams.order}` : null
       }
-  });
-
-  return <div {...others} class={classes}>{children}</div>
-
+  })
+  return <div class={classes} {...others}>{children}</div>
 }
 
 Col.defaultProps = {
   offset: 0
-}
-
-Col.propTypes = {
-  className: PropTypes.string,
-  children: PropTypes.node,
-  span: stringOrNumber,
-  xs: numberOrObject,
-  sm: numberOrObject,
-  md: numberOrObject,
-  lg: numberOrObject,
-  xl: numberOrObject,
-  offset: stringOrNumber,
-  prefix: PropTypes.string,
-  order: stringOrNumber
 }
 
 export default Col;
