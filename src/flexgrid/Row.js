8363c2501008aaf4f458e76b34338a85d70db08d
= <=>
2018-12-14 17:15:21 +0400

    implemented Row Component

--- src/flexgrid/Row.js
+++ src/flexgrid/Row.js
@@ -1,7 +1,33 @@
-import s from './style/index.scss';
+import {h} from '@falconjs.io/falcon'
+import s from './style/index.scss'
 
-let Row = (props)=> {
-  return(
-      <div>{props.children}</div>
-  )
+let Row = ({ justify='start', align='top', className, gutter = 0 , style, children,prefix = 'flcn-grid-row', ...others }) => {
+let classes = ''
+ classes += `${prefix} ` 
+ classes += `${prefix}-${justify} ` || justify+' '
+ classes += `${prefix}-${align} ` || align+' '
+    const rowStyle = gutter > 0 ? Object.assign({}, {
+      marginLeft: gutter / -2,
+      marginRight: gutter / -2,
+    }, style) : style;
+
+
+    const cols = children.map( (col) => {
+      if (!col) {
+        return null;
+      }
+      if (col.props && gutter > 0) {
+        
+        let style ={
+          paddingLeft: gutter / 2 +'px',
+          paddingRight: gutter / 2 +'px'
+        }
+       
+        col.props.style= style
+        }
+        return col
+      
+    });
+    return <div {...others} style={rowStyle} class={classes}>{cols}</div>;
 }
+export default Row;
--- src/index.js
+++ src/index.js
@@ -1,59 +1,78 @@
-import {h, Render, Observable} from '@falconjs.io/falcon'
-import {Col} from './flexgrid'
-
+import {h, Render} from '@falconjs.io/falcon'
+import {Col, Row} from './flexgrid'
 
 let state = {
   gutterPx: 8,
-  colCount: 1,
+  colCount: 12,
   Cols: []
 }
 
+
 const view = state =>{
+  let colCode = ''
 
-let colCode = ''
+  let onGutterChange =(e) =>{
+    state.gutterPx = e.target.value
+    state.Cols = [] //reset
+    render(state)
+  }
 
-let onGutterChange =(e) =>{
-  state.gutterPx = e.target.value
-  render(state)
-}
+  let onColCountChange = (e) =>{
+    state.colCount = e.target.value
+    state.Cols = [] //reset
+    render(state)
+  }
 
-let onColCountChange = (e) =>{
-  state.colCount = e.target.value
-  state.Cols = [] //reset
-  render(state)
-}
+  for (let i =0; i < state.colCount; i++ ){
+    state.Cols.push(
+      <Col key={i.toString()} md={{ span: `${12 / state.colCount}`, order: 1+ i }}>
+            <div style={{ display: 'flex', 'justifyContent': 'center', 'alignItems': 'center', height: '150px', fontSize: '1.5em', background: `${i % 2 === 0 ? 'rgb(30, 197, 228)' : 'rgb(45, 222, 175)'}`, 'color': '#FFF' }}>{i+1} col-4</div>
+      </Col>
+    )
 
-for (let i =0; i < state.colCount; i++ ){
-  state.Cols.push(
-    <Col key={i.toString()} md={{ span: `${12 / state.colCount}`, order: 12 - i }}>
-          <div style={{ display: 'flex', 'justifyContent': 'center', 'alignItems': 'center', height: '150px', fontSize: '1.5em', background: `${i % 2 === 0 ? 'rgb(30, 197, 228)' : 'rgb(45, 222, 175)'}`, 'color': '#FFF' }}>{i+1} col-4</div>
-    </Col>
+  }
+
+  return (
+    <div>
+      <div style={{ marginBottom: 16 }}>
+      <span style={{ marginRight: 6 }}>Gutter (px): </span>
+      <div style={{ width: '50%' }}>
+      <input type="range" id="gutter" onchange={onGutterChange} name="gutter" min="0" max="50"/>
+        <input value={state.gutterPx} oninput={onGutterChange} />
+      </div>
+      <span style={{ marginRight: 6 }}>Column Count:</span>
+      <div style={{ width: '50%' }}>
+      <input type="range" id="col" onchange={onColCountChange} name="col" min="3" max="12" step ="3"/>
+        <input value={state.colCount} oninput={onColCountChange} />
+      </div>
+      </div>
+      <div>
+      <Row gutter={80}>
+        <Col md={{span:3, order:4}}>Column 1</Col>
+        <Col md={{span:3, order:3}}>Column 2</Col>
+        <Col md={{span:3, order:2}}>Column 3</Col>
+        <Col md={{span:3, order:1}}>Column 4</Col>
+      </Row>
+      <Row gutter={80}>
+        <Col md={{span:3, order:1}}>Column 1</Col>
+        <Col md={{span:3, order:2}}>Column 2</Col>
+        <Col md={{span:3, order:3}}>Column 3</Col>
+        <Col md={{span:3, order:4}}>Column 4</Col>
+      </Row>
+
+      </div>
+      <div>
+      <Row gutter={state.gutterPx}>{state.Cols}</Row>
+      </div>    
+    </div>
   )
-  state.Cols.push(
-    <Col md={{ span: 0 }}>
-        <div style={{ display: 'flex', 'justifyContent': 'center', 'alignItems': 'center', height: '150px', fontSize: '1.5em', 'color': '#FFF' }}>col-4</div>
-    </Col>
-  )
-}
 
-
- return (
-   <div>
-    <div style={{ marginBottom: 16 }}>
-    <span style={{ marginRight: 6 }}>Gutter (px): </span>
-    <div style={{ width: '50%' }}>
-      <input oninput={onGutterChange} />
-    </div>
-    <span style={{ marginRight: 6 }}>Column Count:</span>
-    <div style={{ width: '50%' }}>
-      <input oninput={onColCountChange} />
-    </div>
-    </div>
-    {state.Cols}
-    </div>
- ) //{state.Cols}
- }
+} //End of View Component
 
 const render = Render(view, document.getElementById('root') )
 
+
 render(state)
+
+
+
